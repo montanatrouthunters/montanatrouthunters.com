f16f16c9d60fdb63a3b3557e9af79ad647f3b573
Scott Ginn <2660603+ShoGinn@users.noreply.github.com>
2024-06-28 18:32:55 -0400

    refactor: clean up code

--- src/types.d.ts
+++ src/types.d.ts
@@ -149,12 +149,10 @@ export interface Form {
 }
 
 // WIDGETS
-export interface Hero extends Omit<Headline, 'classes'>, Widget {
+export interface Hero extends Omit<Headline, 'classes'>, Omit<Widget, 'isDark' | 'classes'> {
   content?: string;
+  actions?: string | CallToAction[];
   image?: string | unknown;
-  callToAction1?: CallToAction;
-  callToAction2?: CallToAction;
-  isReversed?: boolean;
 }
 
 export interface Stats extends Omit<Headline, 'classes'>, Widget {
